d88390fe072572eb20f903aba094db30e171014b
Piyush Taneja <piyushtaneja0210@gmail.com>
2020-10-12 23:19:26 +0530

    Eureka server initial project

---
+++ mssc-brewery-eureka/src/main/java/com/piyush/springframework/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.piyush.springframework.msscbreweryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsscBreweryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
+	}
+
+}
---
+++ mssc-brewery-eureka/src/test/java/com/piyush/springframework/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.springframework.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
